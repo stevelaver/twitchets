d0ec33d7317c5aeca8a528d360745f630544ef76
Arran Hobson Sayers <32173585+ahobsonsayers@users.noreply.github.com>
2024-09-05 20:00:16 +0100

    Lower refresh time

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func main() {
 	fetchAndProcessTickets(twicketsClient, notificationClient)
 
 	// Create ticker
-	ticker := time.NewTicker(1 * time.Minute)
+	ticker := time.NewTicker(15 * time.Second)
 	defer ticker.Stop()
 
 	// Loop until exit
