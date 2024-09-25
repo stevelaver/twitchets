0dc7bb1dbce86b1c4579cfd33e07dc7bae48d7e1
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2024-09-25 20:46:16 +0100

    Fix TestGetLatestTickets in CI

--- twickets/twickets_test.go
+++ twickets/twickets_test.go
@@ -12,8 +12,6 @@ import (
 )
 
 func TestGetLatestTickets(t *testing.T) {
-	testutils.SkipIfCI(t, "Does not work in CI. Fix")
-
 	_ = godotenv.Load(testutils.ProjectDirectoryJoin(t, ".env"))
 
 	twicketsAPIKey := os.Getenv("TWICKETS_API_KEY")
